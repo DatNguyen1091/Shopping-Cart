675e38f843fb733feb577fe25da368409c8602c3
DatNguyen1091 <nvdat0204@gmail.com>
2023-04-22 23:37:52 +0700

    API CartsController

---
+++ ShoppingCart/ShoppingCart/Controllers/CartsController.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Models;
+using System.Data.SqlClient;
+
+namespace ShoppingCart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartsController : ControllerBase
+    {
+        private readonly string connectionString = "Server=DATNGUYEN\\SQLEXPRESS;Database=ShoppingCart000;Integrated Security=True;";
+
+        [HttpGet]
+        public List<Carts> GetAllCategories()
+        {
+            List<Carts> carts = new List<Carts>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Carts", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Carts model = new Carts();
+                            model.id = (int)reader["id"];
+                            model.uniqueCartId = (string)reader["uniqueCartId"];
+                            model.cartStatus = (string)reader["cartStatus"];                          
+                            model.createdAt = (DateTime)reader["createdAt"];
+                            model.updatedAt = (DateTime)reader["updatedAt"];
+                            carts.Add(model);
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return carts;
+        }
+
+        [HttpGet("{id}")]
+        public Carts GetCartsId(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Carts WHERE id = @id", connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        Carts item = new Carts
+                        {
+                            id = reader.GetInt32(0),
+                            uniqueCartId = reader.GetString(1),
+                            cartStatus = reader.GetString(2),
+                            createdAt = reader.GetDateTime(3),
+                            updatedAt = reader.GetDateTime(4),
+                        };
+                        return item;
+                    }
+                    connection.Close();
+                    return null!;
+                }
+            }
+        }
+
+        [HttpPost]
+        public Carts AddCart(Carts model)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlTransaction transaction = connection.BeginTransaction();
+                var query = "INSERT INTO Carts (uniqueCartId, cartStatus, createdAt, updatedAt) VALUES (@uniqueCartId, @cartStatus, @createdAt, @updatedAt)";
+                try
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection, transaction))
+                    {
+                        model.createdAt = DateTime.Now;
+                        model.updatedAt = DateTime.Now;
+                        command.Parameters.AddWithValue("@uniqueCartId", model.uniqueCartId);
+                        command.Parameters.AddWithValue("@cartStatus", model.cartStatus);
+                        command.Parameters.AddWithValue("@createdAt", model.createdAt);
+                        command.Parameters.AddWithValue("@updatedAt", model.updatedAt);
+                        command.ExecuteNonQuery();
+                    }
+                    transaction.Commit();
+                    connection.Close();
+                    return model;
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    connection.Close();
+                    return null!;
+                }
+            }
+        }
+
+        [HttpPut("{id}")]
+        public Carts UpdateCart(int id, Carts model)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var query = "UPDATE Carts SET uniqueCartId = @uniqueCartId, cartStatus = @cartStatus, createdAt  = @createdAt, updatedAt = @updatedAt WHERE id = @id";
+                using (SqlTransaction transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        using (SqlCommand command = new SqlCommand(query, connection, transaction))
+                        {
+                            model.createdAt = DateTime.Now;
+                            model.updatedAt = DateTime.Now;
+                            command.Parameters.AddWithValue("@id", id);
+                            command.Parameters.AddWithValue("@uniqueCartId", model.uniqueCartId);
+                            command.Parameters.AddWithValue("@cartStatus", model.cartStatus);
+                            command.Parameters.AddWithValue("@createdAt", model.createdAt);
+                            command.Parameters.AddWithValue("@updatedAt", model.updatedAt);
+                            int rows = command.ExecuteNonQuery();
+                            if (rows == 0)
+                            {
+                                transaction.Rollback();
+                                return null!;
+                            }
+                        }
+                        transaction.Commit();
+                        return model;
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        return null!;
+                    }
+                }
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public string RemoveCart(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("DELETE FROM Carts WHERE id = @id", connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    int rows = command.ExecuteNonQuery();
+                    if (rows > 0)
+                    {
+                        return "Item deleted successfully.";
+                    }
+                }
+                connection.Close();
+            }
+            return "Failed to delete item.";
+        }
+    }
+}
