a1c6043f4a597ceb8f12abc1e1d0b26aaad239a0
DatNguyen1091 <nvdat0204@gmail.com>
2023-04-23 14:01:19 +0700

    API CustomerController

---
+++ ShoppingCart/ShoppingCart/Controllers/CustomersController.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Models;
+using System.Data.SqlClient;
+
+namespace ShoppingCart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly string connectionString = "Server=DATNGUYEN\\SQLEXPRESS;Database=ShoppingCart000;Integrated Security=True;";
+
+        [HttpGet]
+        public List<Customers> GetAllCustomers()
+        {
+            List<Customers> customers = new List<Customers>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Customers", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Customers model = new Customers();
+                            model.id = (int)reader["id"];
+                            model.personId = (int)reader["personId"];
+                            model.isDeleted = (bool)reader["isDeleted"];
+                            model.createdAt = (DateTime)reader["createdAt"];
+                            model.updatedAt = (DateTime)reader["updatedAt"];
+                            customers.Add(model);
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return customers;
+        }
+
+        [HttpGet("{id}")]
+        public Customers GetCustomerId(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Customers WHERE id = @id", connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        Customers model = new Customers
+                        {
+                            id = reader.GetInt32(0),
+                            personId = reader.GetInt32(1),
+                            isDeleted = reader.GetBoolean(2),
+                            createdAt = reader.GetDateTime(3),
+                            updatedAt = reader.GetDateTime(4),
+                        };
+                        return model;
+                    }
+                    connection.Close();
+                    return null!;
+                }
+            }
+        }
+
+        [HttpPost]
+        public Customers AddCustomers(Customers model)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlTransaction transaction = connection.BeginTransaction();
+                var query = "INSERT INTO Customers  ( personId, isDeleted, createdAt, updatedAt) VALUES ( @personId, @isDeleted, @createdAt, @updatedAt)";
+                try
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection, transaction))
+                    {
+                        model.createdAt = DateTime.Now;
+                        model.updatedAt = DateTime.Now;
+                        command.Parameters.AddWithValue("@personId", model.personId);
+                        command.Parameters.AddWithValue("@isDeleted", model.isDeleted);
+                        command.Parameters.AddWithValue("@createdAt", model.createdAt);
+                        command.Parameters.AddWithValue("@updatedAt", model.updatedAt);
+                        command.ExecuteNonQuery();
+                    }
+                    transaction.Commit();
+                    connection.Close();
+                    return model;
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    connection.Close();
+                    return null!;
+                }
+            }
+        }
+
+        [HttpPut("{id}")]
+        public Customers UpdateCustomers(int id, Customers model)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var query = "UPDATE Customers SET personId = @personId, isDeleted = @isDeleted, createdAt  = @createdAt, updatedAt = @updatedAt WHERE id = @id";
+                using (SqlTransaction transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        using (SqlCommand command = new SqlCommand(query, connection, transaction))
+                        {
+                            model.createdAt = DateTime.Now;
+                            model.updatedAt = DateTime.Now;
+                            command.Parameters.AddWithValue("@id", id);
+                            command.Parameters.AddWithValue("@personId", model.personId);
+                            command.Parameters.AddWithValue("@isDeleted", model.isDeleted);
+                            command.Parameters.AddWithValue("@createdAt", model.createdAt);
+                            command.Parameters.AddWithValue("@updatedAt", model.updatedAt);
+                            int rows = command.ExecuteNonQuery();
+                            if (rows == 0)
+                            {
+                                transaction.Rollback();
+                                return null!;
+                            }
+                        }
+                        transaction.Commit();
+                        return model;
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        return null!;
+                    }
+                }
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public string RemoveCustomer(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("DELETE FROM Customers WHERE id = @id", connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    int rows = command.ExecuteNonQuery();
+                    if (rows > 0)
+                    {
+                        return "Item deleted successfully.";
+                    }
+                }
+                connection.Close();
+            }
+            return "Failed to delete item.";
+        }
+    }
+}
